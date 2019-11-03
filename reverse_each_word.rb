be4baff6038111c1c7000add116f50332478df4a
Jennifer Prince <jennifergraceprince@gmail.com>
2019-11-03 04:30:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word_with_each(string)
   end
   return_array.join(" ")
 end
+
+def reverse_each_word(string)
+  array = string.split(" ") 
+  test_array = []
+  array.collect do|string| 
+    test_array << string.reverse
+  end
+  test_array.join(" ")
+end
